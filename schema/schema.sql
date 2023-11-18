063cb3146c958f713fb30e1ffcdd53a078af0482
Ashley Frank <ashley.frank@ufl.edu>
2023-11-18 12:52:32 -0500

    added user table to schema file

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,3 +5,30 @@ CREATE TABLE users (
   username VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE user_profiles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  insurance_company VARCHAR(50) NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE allergies (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  medicine_allergy VARCHAR(50) NOT NULL
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE prescriptions (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  medicine_name VARCHAR(50) NOT NULL,
+  dose_mg INT NOT NULL,
+  times_a_day INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
